c342e35c57173138a09994147a5dcd30c2329ada
Miguelangel <dev.miguelangel.jaimen@gmail.com>
2026-05-06 21:41:19 -0400

    auth

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -27,9 +27,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: configService.getOrThrow<string>('GOOGLE_CLIENT_SECRET'),
       callbackURL: configService.getOrThrow<string>('GOOGLE_CALLBACK_URL'),
       scope: ['email', 'profile'],
-      passReqToCallback: false,
       store: new PassthroughStateStore(),
-    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any);
   }
 
   async validate(
